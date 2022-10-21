761b89ebbcae77fe9985851074aa428a14b35916
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-10-21 00:18:05 -0500

    Cambio de base de datos sqlite a postgresql

--- api_rest/app.py
+++ api_rest/app.py
@@ -6,7 +6,7 @@ from models import db
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test@postgres:5432/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY']='secret-key'
 app.config['PROPAGATE_EXCEPTIONS'] = True
