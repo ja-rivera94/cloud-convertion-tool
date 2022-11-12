cbfd75558cce12778779dbef35e8cbd47656cd46
Hawer Alberto Forero Rey <h.foreror@uniandes.edu.co>
2022-11-12 16:13:08 -0500

    Update job_convert_audios.sh

--- api_rest/job_convert_audios.sh
+++ api_rest/job_convert_audios.sh
@@ -59,13 +59,7 @@ for row in "${result[@]}";do
         rm "/tmp/$input_file"
         rm "/tmp/$output_file"        
         PGPASSWORD=test $PSQL -X -h $DB_HOST -U $DB_USER -c "update task set status='processed' where id_task='$id_task'" || true
-        echo 'task already processed' > txtMail.txt
-        curl --ssl-reqd \
-            --url 'smtps://smtp.gmail.com:465' \
-            --user 'minchasrivera@gmail.com:eyfizczllerlvrqu' --ssl-reqd\
-            --mail-from 'minchasrivera@gmail.com' \
-            --mail-rcpt $email \
-            -T txtMail.txt
+        python3 SendMail_API.py "Task processed"
     fi
 
 done
