03ce59716ebceb6752f0a9011f85c448b059309e
Jael Alexander Rivera Oviedo <jael.rivera@192.168.0.5>
2022-11-20 18:53:47 -0500

    include deploy for worker

--- worker/gcp-deploy/startup.sh
+++ worker/gcp-deploy/startup.sh
@@ -4,6 +4,7 @@ sudo apt install python-dev -y
 echo "Installing python-pip"
 sudo apt install python3-pip -y
 sudo apt install git -y
+sudo apt install ffmpeg -y
 echo "cloning repository"
 git clone https://github.com/ja-rivera94/cloud-convertion-tool.git
 cd cloud-convertion-tool/worker/
