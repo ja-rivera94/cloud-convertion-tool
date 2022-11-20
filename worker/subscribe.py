6029c4eb9477bec87767a12af3baabc2132e4a26
Oscar Tobar <o.tobar@uniandes.edu.co>
2022-11-20 02:07:54 -0500

    Se adicionan cambios de postgress

--- worker/subscribe.py
+++ worker/subscribe.py
@@ -19,15 +19,13 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 def get_connection():
-    # connection = psycopg2.connect(user="test", password="test", host="10.127.0.3", port="5432", database="test")
-    connection = sqlite3.connect('D:\\Personal\\MISO\\Onedrive\\OneDrive - Universidad de los Andes\\Trimestre 5\\cloud\\Proyecto\\cloud-convertion-tool\\api_rest\\instance\\data.db')
+    connection = psycopg2.connect(user="test", password="test", host="10.127.0.3", port="5432", database="test")
+    #connection = sqlite3.connect('D:\\Personal\\MISO\\Onedrive\\OneDrive - Universidad de los Andes\\Trimestre 5\\cloud\\Proyecto\\cloud-convertion-tool\\api_rest\\instance\\data.db')
     return connection
 
 def close_connection(connection):
     if connection:
         connection.close()
-        print("Postgres connection is closed")
-        
     
 def convertidor(origen,destino):
     extension = os.path.basename(origen)
@@ -77,8 +75,8 @@ def procesa_tarea(tarea):
         miconexion = get_connection()
         cursor = miconexion.cursor()
         t = (llave,)
-        #select_query = """select t.id_task, t.username, t.filename_input, t.filename_output, u.email,t.status from task as t , public.user as u where t.username::text = u.id::text  and status <> 'uploaded' and  t.id_task::text <> %s"""
-        select_query = """select t.id_task, t.username, t.filename_input, t.filename_output, u.email,t.status from task as t , user as u where t.username = u.id  and status = 'uploaded' and  t.id_task = ?"""
+        select_query = """select t.id_task, t.username, t.filename_input, t.filename_output, u.email,t.status from task as t , public.user as u where t.username::text = u.id::text  and status = 'uploaded' and  t.id_task::text = %s"""
+        #select_query = """select t.id_task, t.username, t.filename_input, t.filename_output, u.email,t.status from task as t , user as u where t.username = u.id  and status = 'uploaded' and  t.id_task = ?"""
         cursor.execute(select_query, t)
         row = cursor.fetchone()
         if row is not None:
@@ -90,35 +88,40 @@ def procesa_tarea(tarea):
             print("email:", row[4])
             print("status",row[5])
             download_blob( "archivos/originales/"+row[2] ,row[2] )
-            #convertidor(row[2] , row[3])
-            #upload_blob( row[3],"archivos/procesados/"+row[3] )
-            upload_blob( row[2],"archivos/procesados/"+row[3] )
+            convertidor(row[2] , row[3])
+            upload_blob( row[3],"archivos/procesados/"+row[3] )
+            #upload_blob( row[2],"archivos/procesados/"+row[3] )
             
             if path.exists(row[2]):
                 remove(row[2])
                 
-            #if path.exists(row[3]):    
-            #   remove(row[3])
+            if path.exists(row[3]):    
+               remove(row[3])
             update_query = """update task set status='processed' where id_task= ?"""
             cursor.execute(update_query, t)
             miconexion.commit()
             cursor.close()
-            print("Record Updated successfully ")
+            
         close_connection(miconexion)
-    #except (Exception, psycopg2.Error) as error:
-    except (Exception, sqlite3.Error) as error:
+        return True
+    except (Exception, psycopg2.Error) as error:
+    #except (Exception, sqlite3.Error) as error:
         print("Error trayendo datos. ", error)
+        return False
     finally:
         if miconexion:
             miconexion.close()
-            print("The SQLite connection is closed")
+        print("Fin del proceso")
      
     
     
 def callback(message):
     print("Received message: {}".format(message.data))
-    procesa_tarea(str(message.data.decode("utf-8")))
-    message.ack()
+    if procesa_tarea(str(message.data.decode("utf-8"))):
+        message.ack()
+        print("Mensaje Procesado correctamente")
+    else:
+        print("Mensaje Pendiente")
     
     
     
