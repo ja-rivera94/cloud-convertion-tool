2119f714e86b6c3a3e59467724967a8a0922f3cc
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-12-01 08:10:28 -0500

    worker service app engine

---
+++ worker/main.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def status():
+    return 'Worker working'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
